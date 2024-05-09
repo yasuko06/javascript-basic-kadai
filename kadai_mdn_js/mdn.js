6070c77a474c73908a1f1d2888c2d7319f0a8aa0
yasuko06 <153053254+yasuko06@users.noreply.github.com>
2024-05-10 04:01:56 +0900

    Add files via upload
    課題MDN

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const now = new Date([2023, 8, 22]);
+
+// 年月日を1つの文字列に結合し、「年」「月」「日」を付加
+const formattedDate = now.getFullYear() + '年' + 
+                      (now.getMonth() + 1) + '月' + 
+                      now.getDate() + '日';
+
+console.log(formattedDate);
